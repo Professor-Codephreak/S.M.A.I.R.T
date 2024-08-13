7a308f02552ad20312fb72627245d6f10fd4a998
codephreak <codephreak@dmg.finance>
2024-08-13 15:05:08 -0700

    Add files via upload

--- UIUX/utils/config.ts
+++ UIUX/utils/config.ts
@@ -1,1 +1,6 @@
+// utils/config.ts
+export const PRESALE_CONTRACT_ADDRESS = process.env.NEXT_PUBLIC_PRESALE_CONTRACT_ADDRESS || "";
+if (!PRESALE_CONTRACT_ADDRESS) {
+  throw new Error("Missing presale contract address in environment variables.");
+}
 
