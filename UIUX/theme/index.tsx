0460e959c8667776958cdde9c1f7722053325463
codephreak <codephreak@dmg.finance>
2024-08-13 15:04:23 -0700

    Add files via upload

--- UIUX/theme/index.tsx
+++ UIUX/theme/index.tsx
@@ -1,1 +1,18 @@
+// theme/index.ts
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  styles: {
+    global: {
+      "html, body": {
+        fontFamily: "'Inter', sans-serif",
+        color: "gray.800",
+        bg: "gray.100",
+        lineHeight: "tall",
+      },
+    },
+  },
+});
+
+export default theme;
 
